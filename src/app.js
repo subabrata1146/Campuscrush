9337ea3ed2392efe1a2016e7032f2ee704bc2a25
subabrata1146 <140155135+subabrata1146@users.noreply.github.com>
2025-06-23 23:44:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import SwipePage from "./pages/SwipePage";
+import Profile from "./pages/Profile";
+import Chat from "./pages/Chat";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SwipePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
