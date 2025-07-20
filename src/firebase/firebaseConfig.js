6a038500b891187dc8a5bb2c27da7fececc34394
subabrata1146 <140155135+subabrata1146@users.noreply.github.com>
2025-07-20 20:27:00 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBWO15LGY8dWZlcSVUPUS6l72apaKVbM7c",
   authDomain: "campuscrush-b18d6.firebaseapp.com",
   projectId: "campuscrush-b18d6",
-  storageBucket: "campuscrush-b18d6.firebasestorage.app",
+    storageBucket: "campuscrush-b18d6.appspot.com",
   messagingSenderId: "776700696889",
   appId: "1:776700696889:web:d6d8ef9344a78dc33e30e8",
 };
